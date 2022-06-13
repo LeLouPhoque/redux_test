6f32685174aab4c54348dbe54894b10aa6f9795d
Esteban <90008993+LeLouPhoque@users.noreply.github.com>
2022-06-13 09:18:34 +0200

    Add files via upload

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import UserReducer from './reducer_users';
+import ActiveUserReducer from './reducer_activeUser'
+const rootReducer = combineReducers({
+  users : UserReducer,
+  activeUser : ActiveUserReducer
+});
+
+export default rootReducer;
